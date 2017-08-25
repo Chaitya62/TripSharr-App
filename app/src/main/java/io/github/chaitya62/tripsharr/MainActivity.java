22498e6f4b48c335394babdcc3794d2f4c1ef28c
Ankit-22 <ankitmani.t@somaiya.edu>
2017-08-25 11:26:56 +0530

    Add Login

--- app/src/main/java/io/github/chaitya62/tripsharr/MainActivity.java
+++ app/src/main/java/io/github/chaitya62/tripsharr/MainActivity.java
@@ -1,13 +1,57 @@
 package io.github.chaitya62.tripsharr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String authToken, userId, message;
+    private CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                userId = loginResult.getAccessToken().getUserId();
+                authToken = loginResult.getAccessToken().getToken();
+                message = "login success";
+                Intent intent = new Intent(getApplication(), HomeActivity.class);
+                intent.putExtra("userId", userId);
+                intent.putExtra("authToken", authToken);
+                startActivity(intent);
+                Log.i("LoginAttempt", message);
+            }
+            @Override
+            public void onCancel() {
+                message = "Login attempt canceled.";
+                Log.i("LoginAttempt", message);
+            }
+            @Override
+            public void onError(FacebookException e) {
+                message = "Login attempt failed.";
+                Log.i("LoginAttempt", message);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
+
