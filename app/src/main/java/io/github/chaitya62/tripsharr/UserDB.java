5a2df58b51f8b26385af8e6bd6bbcbf6f8a6ad8a
Ankit-22 <ankitmani.t@somaiya.edu>
2017-08-27 17:32:50 +0530

    Resolve Conflicts

--- app/src/main/java/io/github/chaitya62/tripsharr/UserDB.java
+++ app/src/main/java/io/github/chaitya62/tripsharr/UserDB.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import io.github.chaitya62.tripsharr.primeObjs.User;
 
 /**
  * Created by ankit on 26/8/17.
