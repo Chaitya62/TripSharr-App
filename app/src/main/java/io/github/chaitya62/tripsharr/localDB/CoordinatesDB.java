505fe6253d05a8e8f81f4983a9c936d85879ba5e
Ankit-22 <ankitmani.t@somaiya.edu>
2017-08-30 10:58:02 +0530

    Media

---
+++ app/src/main/java/io/github/chaitya62/tripsharr/localDB/CoordinatesDB.java
@@ -0,0 +1,8 @@
+package io.github.chaitya62.tripsharr.localDB;
+
+/**
+ * Created by ankit on 29/8/17.
+ */
+
+public class CoordinatesDB {
+}
---
+++ app/src/main/java/io/github/chaitya62/tripsharr/primeobjects/Media.java
@@ -0,0 +1,8 @@
+package io.github.chaitya62.tripsharr.primeobjects;
+
+/**
+ * Created by ankit on 29/8/17.
+ */
+
+public class Media {
+}
